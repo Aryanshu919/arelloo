ab41f43aa2fb3aa00842c02a80f05d279dbe414d
Aryan Kumar <aryanshu919@gmail.com>
2025-06-08 11:06:24 +0530

    first commit

---
+++ server/src/app.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://meditrack-frontend.vercel.app"
+];
+
+const corsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void,
+  ) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Requested-With",
+    "Accept",
+    "Origin",
+  ],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+export default app;
